c8946655a8753c00ac597209ebf9f8f734785276
Yuan Zhi <yuan.zhi@citrix.com>
2017-05-02 18:11:27 -0400

    auth policy

--- config/policies.js
+++ config/policies.js
@@ -28,6 +28,11 @@ module.exports.policies = {
 
   '*': 'isLoggedIn',
 
+  'AuthController': {
+      
+      '*' : 'true'
+      
+  },
   /***************************************************************************
   *                                                                          *
   * Here's an example of mapping some policies to run before a controller    *
